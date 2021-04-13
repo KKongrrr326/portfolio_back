72dfe9e23b1a7576e6a52a1f2ceffd7fa03cb08d
KKongrrr326 <52910771+KKongrrr326@users.noreply.github.com>
2021-04-14 02:46:06 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* main.js */
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        autoScrolling:true,
+        scrollHorizontally: true,
+        keyboardScrolling : true, 
+        navigation: true,
+        navigationTooltips : ['Home','jisign','mintech','admin','design','contact'],
+        navigationPosition: 'right',
+        showActiveTooltips : true,
+        scrollingSpeed : 500,
+        afterResponsive : function(isResponsive){
+            if($(window).width() < 1024){ $('#fullpage').removeAttr('id'); }
+        }
+
+    });
+});
+
+// $(function() {
+
+//     if($(window).width() < 1024){ $('#fullpage').removeAttr('id'); }
+
+// });
